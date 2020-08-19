7dddf50b9cc44afa63349b42365d82769fc9ac7b
Matthew C. Pilsbury <matthewpilsbury@protonmail.com>
2020-08-19 19:42:43 -0400

    Remove mbedtls add python3-http-parser

--- setup.sh
+++ setup.sh
@@ -6,15 +6,12 @@ fi
 apt update --yes
 apt dist-upgrade --yes
 apt upgrade --yes
-apt install --yes git python3 python3-pip python3-requests python3-http-parser python3-dev libpython-dev libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
+apt install --yes git python3 python3-pip python3-requests python3-http-parser python3-dev libpython-dev libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev python3-http-parser screen
 echo "############### BASE DEPENDENCIES DONE - CHECK FOR ERRORS ###############" 
 apt --yes install python3-opencv
 echo "############### OPENCV DONE - CHECK FOR ERRORS ###############" 
+apt autoremove --yes
+apt clean
 echo "############### Check our GitHub for alternate install instructions if you error above. ###############" 
-git clone https://github.com/Synss/python-mbedtls
-cd python-mbedtls
-pip3 install -r requirements.txt
-python3 ./setup.py install
-echo "############### MBEDTLS DONE - CHECK FOR ERRORS ###############" 
 echo "############### FULLY COMPLETE - CHECK FOR ERRORS ###############" 
 exit
